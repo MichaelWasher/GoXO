0a7b050ebeaff569da10a935acfa2ed04d7354f2
Michael Washer <michael.washer@icloud.com>
2021-01-06 11:56:02 +1300

    Implement gRPC Server
    Add basic gRPC Server (not incorporated with game yet)

---
+++ gamesocket.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+// TODO Rewrite this to be clean
+func setup_server(portNum int){
+	// create a listener on TCP port 7777
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", portNum))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	// create a server instance
+	s := Server{}
+	// create a gRPC server object
+	grpcServer := grpc.NewServer()
+	// attach the Ping service to the server
+	reflection.Register(grpcServer)
+	RegisterMoveServiceServer(grpcServer, &s)
+	// start the server
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
+
+// TODO Rewrite this to be clean
+func setup_client(portNum int){
+
+}
---
+++ grpc-server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+	"log"
+)
+
+// Server represents the gRPC server
+type Server struct {
+}
+
+// PerformMove generates response to a Ping request
+func (s *Server) PerformMove(ctx context.Context, in *MoveMessage) (*Empty, error) {
+	log.Printf("Receive message %s", in.MoveRequest)
+	// Write to the Channel // outstandingMoves <- in.MoveRequest
+	return &Empty{}, nil
+}
